503755fba9e872539d0d9ee782752e906bcf3cbe
Gerd Lödige <gerd.loedige@posteo.de>
2023-06-01 11:39:44 +0200

    create class MenusRepository with getAllMenus and getSingleMenuById

--- src/main/java/de/iav/burgershop/repository/MenuesRepository.java
+++ src/main/java/de/iav/burgershop/repository/MenuesRepository.java
@@ -1,2 +1,39 @@
-package de.iav.burgershop.repository;public class MenuesRepository {
+package de.iav.burgershop.repository;
+
+import de.iav.burgershop.exception.MenueNotFoundException;
+import de.iav.burgershop.model.Menus;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MenuesRepository {
+
+    private List<Menus> menus;
+
+    public MenuesRepository(){
+        menus = new ArrayList<>();
+    }
+
+    public MenuesRepository(List<Menus> menus) {
+        this.menus = menus;
+    }
+
+    public List<Menus> getAllMenus() {
+        return menus;
+    }
+
+    public Menus getSingleMenuById(String id){
+        for(Menus singleMenuFromList : menus){
+            if(singleMenuFromList.id().equals(id)){
+                return singleMenuFromList;
+            }
+        }
+        throw new MenueNotFoundException(id);
+    }
+
+    public void addMenu(Menus menuToAdd){
+        menus.add(menuToAdd);
+    }
+
+
 }
