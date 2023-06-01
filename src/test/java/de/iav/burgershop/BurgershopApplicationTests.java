2a0db4eea9cb110cfab19ca580bc85b1e5d07c0c
Gerd Lödige <gerd.loedige@posteo.de>
2023-06-01 10:23:49 +0200

    initial commit

---
+++ src/test/java/de/iav/burgershop/BurgershopApplicationTests.java
@@ -0,0 +1,13 @@
+package de.iav.burgershop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BurgershopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
