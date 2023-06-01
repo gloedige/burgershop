5aa2488d559ea868edd0175f8d83d88b5a9a40f3
Gerd Lödige <gerd.loedige@posteo.de>
2023-06-01 12:45:41 +0200

    create methodes in BurgerShopService of MenusRepository

--- src/main/java/de/iav/burgershop/service/BurgerShopService.java
+++ src/main/java/de/iav/burgershop/service/BurgerShopService.java
@@ -1,10 +1,22 @@
 package de.iav.burgershop.service;
 
+import de.iav.burgershop.model.Menu;
 import de.iav.burgershop.repository.MenusRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BurgerShopService {
     private MenusRepository menusRepository;
+
+    public BurgerShopService(MenusRepository menusRepository) {
+        this.menusRepository = menusRepository;
+    }
+    public List<Menu> getAllMenus() {return menusRepository.getAllMenus(); }
+    public Menu getSingleMenuById(String id) {return menusRepository.getSingleMenuById(id); }
+    public void addMenu(Menu menuToAdd) { menusRepository.addMenu(menuToAdd);}
+    public void updateMenu(Menu otherMenu, String id){menusRepository.updateMenu(otherMenu, id);}
+    public void deleteMenu(String id){menusRepository.deleteMenuById(id);}
 
 }
