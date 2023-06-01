f3848dcd0b93ca80a842585677702e3a07b8b6fe
Gerd Lödige <gerd.loedige@posteo.de>
2023-06-01 12:32:17 +0200

    create Service BurgerShopService

---
+++ src/main/java/de/iav/burgershop/service/BurgerShopService.java
@@ -0,0 +1,10 @@
+package de.iav.burgershop.service;
+
+import de.iav.burgershop.repository.MenusRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BurgerShopService {
+    private MenusRepository menusRepository;
+
+}
