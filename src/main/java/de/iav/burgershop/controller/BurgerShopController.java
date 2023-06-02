e5dbe76f651554e249a7fb1e61e24ac38e42275a
Gerd Lödige <gerd.loedige@posteo.de>
2023-06-02 09:33:20 +0200

    edit BrugerShopController with all methodes

--- src/main/java/de/iav/burgershop/controller/BurgerShopController.java
+++ src/main/java/de/iav/burgershop/controller/BurgerShopController.java
@@ -1,8 +1,10 @@
 package de.iav.burgershop.controller;
 
+import de.iav.burgershop.model.Menu;
 import de.iav.burgershop.service.BurgerShopService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/burgershop")
@@ -13,5 +15,18 @@ public class BurgerShopController {
     public BurgerShopController(BurgerShopService burgerShopService) {
         this.burgerShopService = burgerShopService;
     }
+    @GetMapping("/Menus")
+    public List<Menu> getAllMenus(){return burgerShopService.getAllMenus();}
 
+    @GetMapping("Menus/{id}")
+    Menu getSingleMenuById(String id){return burgerShopService.getSingleMenuById(id);}
+
+    @PostMapping ("/Menus")
+    public void addMenu(Menu menuToAdd){burgerShopService.addMenu(menuToAdd);}
+
+    @PostMapping ("/Menus/{id}")
+    public void updateMenu(Menu otherMenu, String id){burgerShopService.updateMenu(otherMenu, id);}
+
+    @DeleteMapping ("/Menus")
+    public void deleteMenu(String id){burgerShopService.deleteMenu(id);}
 }
